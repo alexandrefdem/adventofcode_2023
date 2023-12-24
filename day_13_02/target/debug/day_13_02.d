0e28541b44d8044c94a96aa2fbf9b6abf6cd723e
Alexandre de Magalhaes <alexandredemagalhaess@gmail.com>
2023-12-24 14:16:25 -0300

    latest

---
+++ day_13_02/target/debug/day_13_02.d
@@ -0,0 +1,1 @@
+/Users/alexandredemagalhaes/repos/adventofcode_2023/day_13_02/target/debug/day_13_02: /Users/alexandredemagalhaes/repos/adventofcode_2023/day_13_02/src/main.rs
---
+++ day_13_02/target/debug/deps/day_13_02-bf062fc8fce7437e.d
@@ -0,0 +1,5 @@
+/Users/alexandredemagalhaes/repos/adventofcode_2023/day_13_02/target/debug/deps/day_13_02-bf062fc8fce7437e: src/main.rs
+
+/Users/alexandredemagalhaes/repos/adventofcode_2023/day_13_02/target/debug/deps/day_13_02-bf062fc8fce7437e.d: src/main.rs
+
+src/main.rs:
